abab3c2e0b99bd9b58b98a7321c6cc7e43c6a909
Matheus Massari <massari.matheus@gmail.com>
2021-08-26 18:00:29 -0300

    atualizando

--- 01-github-explorer/src/index.jsx
+++ 01-github-explorer/src/index.jsx
@@ -1,11 +1,6 @@
-import React from 'react';
+import { render } from 'react-dom';
+import { App } from './App.jsx'
 
-const user = {
-    name: 'Matheus',
-}
-
-console.log(user.adress?.street)
-
-function App() {
-    return <h1>Hello World</h1>
-}
+render(
+    < App />, document.getElementById('root')
+ )
